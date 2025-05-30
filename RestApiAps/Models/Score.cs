1d3cddc25ebceb4be73135c67a319022b0cb77fd
Pedro Borges <pedro.bs10@icloud.com>
2025-05-29 23:10:22 -0300

    teste4

--- RestApiAps/Models/Score.cs
+++ RestApiAps/Models/Score.cs
@@ -10,7 +10,7 @@ namespace RestApiAps.Models
         [Key]
         public int Id { get; set; }
 
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
 
         public int Pontuacao { get; set; }
 
